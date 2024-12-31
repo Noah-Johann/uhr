01cea21d0e6c8e37c47e36d8c24031118975654b
Noah <179856882+Noah-Johann@users.noreply.github.com>
2024-12-31 14:00:13 +0100

    Darkmode-Switch

--- uhr1.py
+++ uhr1.py
@@ -1,12 +1,11 @@
 import pygame, time
 from pygame.locals import *
 from math import sin, cos, radians
+
 pygame.init()
 
-Rot = (255,0,0); Weiss = (255, 255, 255); Schwarz = (0, 0, 0)
+Rot = (255,0,0); Background = (255, 255, 255); UI = (0, 0, 0)
 
-Feld = pygame.display.set_mode((400, 400))
-Feld.fill(Weiss)
 MX = 200; MY = 200; MP = ((MX, MY))
 
 def punkt(A, W):
@@ -14,11 +13,17 @@ def punkt(A, W):
     y1 = int(MY + A * sin(w1))
     return ((x1, y1))
 
-for i in range(60):
-    pygame.draw.circle(Feld, Schwarz, punkt(190, i), 2) 
+def UI_Draw():
+    for i in range(60):
+        pygame.draw.circle(Feld, UI, punkt(190, i), 2) 
 
-for i in range(12):
-    pygame.draw.circle(Feld, Schwarz, punkt(190, i*5), 4)
+    for i in range(12):
+        pygame.draw.circle(Feld, UI, punkt(190, i*5), 4)
+
+Feld = pygame.display.set_mode((400, 400))
+Feld.fill(Background)
+UI_Draw()
+
 
 mainloop = True
 s1 = 0
@@ -31,14 +36,30 @@ while mainloop:
         h = h - 12
 
     hm=(h + m / 60.0) * 5
-    pygame.draw.circle(Feld, Weiss, MP, 182)
-    pygame.draw.line(Feld, Schwarz, MP, punkt(120, hm), 6)
-    pygame.draw.line(Feld, Schwarz, MP, punkt(170, m), 4)
+    pygame.draw.circle(Feld, Background, MP, 182)
+    pygame.draw.line(Feld, UI, MP, punkt(120, hm), 6)
+    pygame.draw.line(Feld, UI, MP, punkt(170, m), 4)
     pygame.draw.line(Feld, Rot, MP, punkt(180, s), 2)
+
+    
         
     s1 = pygame.display.update()
 
     for event in pygame.event.get():
+
+        #Dark Mode
+        if event.type == KEYUP and event.key == K_d:
+            if Background == (255, 255, 255):
+                Background = (0, 0, 0)
+                UI = (255, 255, 255)
+            else:
+                Background = (255, 255, 255) 
+                UI = (0, 0, 0)
+
+            Feld.fill(Background)
+            UI_Draw()
+
+        #Quit
         if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
             mainloop = False
 
